b0c90aa65a15f444f69470bf72b9b3159173208b
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-07-24 17:00:44 +0200

    doc update

--- search/all_19.js
+++ search/all_19.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['y',['y',['../class_gtk_1_1_p_lplot_1_1_plot2_d_data.html#a35c755bc48d51c2312b627f192275527',1,'Gtk::PLplot::Plot2DData']]],
-  ['year',['Year',['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Date.html#aa308aa855887d94a371adddfc23353c7',1,'Glib::Date::Year()'],['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Date.html#gac803072d86bbc2ce1f7d558c94f41ae1a9f9e923dc13c924a74987cf00250674e',1,'Glib::Date::YEAR()']]],
+  ['year',['YEAR',['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Date.html#gac803072d86bbc2ce1f7d558c94f41ae1a9f9e923dc13c924a74987cf00250674e',1,'Glib::Date::YEAR()'],['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Date.html#aa308aa855887d94a371adddfc23353c7',1,'Glib::Date::Year()']]],
   ['yes',['YES',['http://library.gnome.org/devel/gtkmm/unstable/namespaceGtk_1_1Stock.html#acf80d9c8e5271cf1e449060c44d714da',1,'Gtk::Stock']]],
   ['yield',['yield',['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Thread.html#acad8be7a8093ef3aca32344264461011',1,'Glib::Thread::yield()'],['http://library.gnome.org/devel/glibmm/unstable/classGlib_1_1Threads_1_1Thread.html#a0b4e6d3fe278429716f0e0cca3654fde',1,'Glib::Threads::Thread::yield()'],['http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a01629.html#a2a1ecebfe73aaa3eb40e909991bbd16e',1,'std::this_thread::yield()']]]
 ];
