7afd51c27dd9711ee97be90d8ea9a1807dc5cfd4
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-07-27 15:36:31 +0200

    travis-ci: further fixes...

--- travis-before-install-linux.sh
+++ travis-before-install-linux.sh
@@ -1,16 +1,16 @@
-sudo add-apt-repository ppa:kalakris/cmake -y
+sudo add-apt-repository ppa:kalakris/cmake -y || exit 1
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y || exit 1
 
-sudo apt-get update
+sudo apt-get update || exit 1
 
-sudo apt-get install -y libffi-dev mm-common autoconf automake libtool doxygen pkg-config cmake libpng12-dev gettext libfreetype6-dev libfontconfig1-dev libjpeg-dev libtiff4-dev intltool libxtst-dev
+sudo apt-get install -y libffi-dev mm-common autoconf automake libtool doxygen pkg-config cmake libpng12-dev gettext libfreetype6-dev libfontconfig1-dev libjpeg-dev libtiff4-dev intltool libxtst-dev || exit 1
 
 
 
 if [ $CXX == "g++" ] ; then
-	sudo apt-get install -y g++4.9
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
+	sudo apt-get install -y g++4.9 || exit 1
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50 || exit 1
 elif [ $CXX == "clang++"] ; then
-	sudo apt-get install -y clang
+	sudo apt-get install -y clang || exit 1
 fi
