86460b6cae219e7a31de9e3e1e55a7c656f32150
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-07-27 16:01:54 +0200

    travis-ci: another typo fixed

--- travis-before-install-linux.sh
+++ travis-before-install-linux.sh
@@ -11,6 +11,6 @@ sudo apt-get install -y libffi-dev mm-common autoconf automake libtool doxygen p
 if [ $CXX == "g++" ] ; then
 	sudo apt-get install -y g++-4.9 || exit 1
 	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50 || exit 1
-elif [ $CXX == "clang++"] ; then
+elif [ $CXX == "clang++" ] ; then
 	sudo apt-get install -y clang || exit 1
 fi
