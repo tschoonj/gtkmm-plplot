acd0464a0078056b99118e9dd9452bcd75f1f57e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-06-22 16:56:38 +0200

    PlotData2DErrorY: new class

---
+++ gtkmm-plplot/plotdata2derrory.cpp
@@ -0,0 +1,144 @@
+/*
+Copyright (C) 2016 Tom Schoonjans
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtkmm-plplot/plotdata2derrory.h>
+#include <gtkmm-plplot/exception.h>
+#include <gtkmm-plplot/utils.h>
+
+using namespace Gtk::PLplot;
+
+PlotData2DErrorY::PlotData2DErrorY(
+  const std::vector<double> &_x,
+  const std::vector<double> &_y,
+  const std::vector<double> &_errory_low,
+  const std::vector<double> &_errory_high,
+  Gdk::RGBA _color,
+  LineStyle _line_style,
+  double _line_width) :
+  Glib::ObjectBase("GtkmmPLplotPlotData2DErrorY"),
+  PlotData2D(_x, _y, _color, _line_style, _line_width),
+  errory_low(_errory_low),
+  errory_high(_errory_high),
+  errory_color(_color) {
+
+  //ensure all arrays have the same size
+  if (y.size() != _errory_low.size() || y.size() != _errory_high.size()) {
+    throw Exception("Gtk::PLplot::PlotData2DErrorY::PlotData2DErrorY -> data arrays x, y, errory_low and errory_high must have the same size!");
+  }
+
+  //ensure that the errory_low values are greater than y
+  for (int i = 0 ; i < y.size() ; i++) {
+    if (errory_low[i] > y[i])
+      throw Exception("Gtk::PLplot::PlotData2DErrorY::PlotData2DErrorY -> errory_low elements must be less than the corresponding elements in y");
+    else if (errory_high[i] < y[i])
+      throw Exception("Gtk::PLplot::PlotData2DErrorY::PlotData2DErrorY -> errory_high elements must be greater than the corresponding elements in y");
+  }
+}
+
+PlotData2DErrorY::PlotData2DErrorY(
+  const std::valarray<double> &_x,
+  const std::valarray<double> &_y,
+  const std::valarray<double> &_errory_low,
+  const std::valarray<double> &_errory_high,
+  Gdk::RGBA _color,
+  LineStyle _line_style,
+  double _line_width) :
+  PlotData2DErrorY(
+    std::vector<double>(std::begin(_x), std::end(_x)),
+    std::vector<double>(std::begin(_y), std::end(_y)),
+    std::vector<double>(std::begin(_errory_low), std::end(_errory_low)),
+    std::vector<double>(std::begin(_errory_high), std::end(_errory_high)),
+    _color, _line_style, _line_width) {}
+
+PlotData2DErrorY::~PlotData2DErrorY() {}
+
+void PlotData2DErrorY::add_datapoint(double _x, double _y) {
+  throw Exception("Gtk::PLplot::PlotData2DErrorY::add_datapoint -> This method is not supported for PlotData2DErrorY");
+}
+
+void PlotData2DErrorY::add_datapoint(std::pair<double, double> _xy_pair) {
+  throw Exception("Gtk::PLplot::PlotData2DErrorY::add_datapoint -> This method is not supported for PlotData2DErrorY");
+}
+
+std::vector<double> PlotData2DErrorY::get_vector_error_y_low() {
+  return errory_low;
+}
+
+std::vector<double> PlotData2DErrorY::get_vector_error_y_high() {
+  return errory_high;
+}
+
+void PlotData2DErrorY::get_extremes(double &xmin, double &xmax, double &ymin, double &ymax) {
+  xmin = *std::min_element(x.begin(), x.end());
+  xmax = *std::max_element(x.begin(), x.end());
+  ymin = *std::min_element(errory_low.begin(), errory_low.end());
+  ymax = *std::max_element(errory_high.begin(), errory_high.end());
+}
+
+void PlotData2DErrorY::set_error_y_color(Gdk::RGBA _errory_color) {
+  if (_errory_color == errory_color)
+    return;
+  errory_color = _errory_color;
+  _signal_changed.emit();
+}
+
+Gdk::RGBA PlotData2DErrorY::get_error_y_color() {
+  return errory_color;
+}
+
+void PlotData2DErrorY::add_datapoint(double xval, double yval, double yval_error_low, double yval_error_high) {
+  // sanity check
+  if (yval_error_low > yval)
+    throw Exception("Gtk::PLplot::PlotData2DErrorY::add_datapoint -> yval_error_low elements must be less than yval");
+  else if (yval_error_high < yval)
+    throw Exception("Gtk::PLplot::PlotData2DErrorY::add_datapoint -> yval_error_high elements must be greater than yval");
+
+  x.push_back(xval);
+  y.push_back(yval);
+  errory_low.push_back(yval_error_low);
+  errory_high.push_back(yval_error_high);
+  _signal_data_modified.emit();
+}
+
+void PlotData2DErrorY::draw_plot_data(const Cairo::RefPtr<Cairo::Context> &cr, plstream *pls) {
+  if (!is_showing())
+    return;
+
+  double *x_pl = &x[0], *y_pl = &y[0];
+  double *errory_low_pl = &errory_low[0];
+  double *errory_high_pl = &errory_high[0];
+
+  // plot the line if requested
+  if (line_style != LineStyle::NONE) {
+    change_plstream_color(pls, color);
+    pls->lsty(line_style);
+    pls->width(line_width);
+    pls->line(x.size(), x_pl, y_pl);
+  }
+
+  // plot the y errorbars
+  change_plstream_color(pls, errory_color);
+  // need to see what happens with the errorbars when changing the line_width and line_style
+  pls->erry(x.size(), x_pl, errory_low_pl, errory_high_pl);
+
+  // plot the symbols if requested
+  if (!symbol.empty()) {
+    change_plstream_color(pls, symbol_color);
+    pls->schr(0, symbol_scale_factor);
+    pls->string(x.size(), x_pl, y_pl, symbol.c_str());
+  }
+}
---
+++ gtkmm-plplot/plotdata2derrory.h
@@ -0,0 +1,157 @@
+/*
+Copyright (C) 2016 Tom Schoonjans
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GTKMM_PLPLOT_PLOTDATA2D_ERRORY_H
+#define GTKMM_PLPLOT_PLOTDATA2D_ERRORY_H
+
+#include <gtkmm-plplot/plotdata2d.h>
+
+namespace Gtk {
+  namespace PLplot {
+    /** \class PlotData2DErrorY plotdata2derrory.h <gtkmm-plplot/plotdata2derrory.h>
+     *  \brief a class that will hold a single XY dataset with Y errorbars and its properties for a Plot2D plot
+     *
+     *  Instances of this class contain a single dataset for a Plot2D two-dimensional plot,
+     *  consisting of the raw X- and Y-data, as well as error values for the Y-data,
+     *  along with a number of properties that will determine
+     *  the appearance of the plot. Data may be represented as a line, symbols or both.
+     *  The constructors of this class allow to use either std::vector or std::valarray as sources of data,
+     *  for added flexibility. Internally they are stored only as std::vector though.
+     *  Important is that whenever a property is changed, \c signal_changed() is emitted, which will eventually
+     *  be picked up by the \c canvas that will hold the plot.
+     *  Several of the methods that are offered by this class are demonstrated in \ref example11
+     */
+    class PlotData2DErrorY : virtual public PlotData2D {
+    private:
+      PlotData2DErrorY() = delete; ///< no default constructor
+      PlotData2DErrorY &operator=(const PlotData2DErrorY &) = delete; ///< no assignment operator
+      PlotData2DErrorY(const PlotData2DErrorY &source) = delete; ///< no default copy constructor;
+      virtual void add_datapoint(double xval, double yval) final; ///< disable this method
+      virtual void add_datapoint(std::pair<double, double> xy_pair) final; ///< disable this method;
+    protected:
+      std::vector<double> errory_low; ///< The lower error margins of the Y-data
+      std::vector<double> errory_high; ///< The upper error margins of the Y-data
+      Gdk::RGBA errory_color; ///< The color of the Y-data errorbars
+    public:
+      /** Constructor
+       *
+       * This constructor initializes a new dataset using four vectors, one for the X-values, one for the Y-values,
+       * and two vectors to define the errorsbar of the Y-data.
+       * Optionally, one can also use the constructor to override the default color, line style and line width.
+       * By default, the color of the errorbars will be the same as the one chosen at construction time for the line.
+       * Override this by calling set_error_y_color() after constructing the dataset.
+       * \param x the X-values, as std::vector
+       * \param y the Y-values, as std::vector
+       * \param errory_low the lower values of the Y-data errorbars. Each value must be less than its Y-data counterpart.
+       * \param errory_high the upper values of the Y-data errorbars. Each value must be greater than its Y-data counterpart.
+       * \param color the line color, default is red
+       * \param line_style the line style, default is CONTINUOUS
+       * \param line_width the line width, default is 1.0
+       * \exception Gtk::PLplot::Exception
+       */
+      PlotData2DErrorY(const std::vector<double> &x,
+                       const std::vector<double> &y,
+                       const std::vector<double> &errory_low,
+                       const std::vector<double> &errory_high,
+                       Gdk::RGBA color = Gdk::RGBA("red"),
+                       LineStyle line_style = CONTINUOUS,
+                       double line_width = PLOTDATA_DEFAULT_LINE_WIDTH);
+
+      /** Constructor
+       *
+       * This constructor initializes a new dataset using four valarrays, one for the X-values, one for the Y-values,
+       * and two vectors to define the errorsbar of the X-data.
+       * Optionally, one can also use the constructor to override the default color, line style and line width.
+       * By default, the color of the errorbars will be the same as the one chosen at construction time for the line.
+       * Override this by calling set_error_x_color() after constructing the dataset.
+       * \param x the X-values, as std::valarray
+       * \param y the Y-values, as std::valarray
+       * \param errory_low the lower values of the Y-data errorbars. Each value must be less than its Y-data counterpart.
+       * \param errory_high the upper values of the Y-data errorbars. Each value must be greater than its Y-data counterpart.
+       * \param color the line color, default is red
+       * \param line_style the line style, default is CONTINUOUS
+       * \param line_width the line width, default is 1.0
+       */
+      PlotData2DErrorY(const std::valarray<double> &x,
+                       const std::valarray<double> &y,
+                       const std::valarray<double> &errory_low,
+                       const std::valarray<double> &errory_high,
+                       Gdk::RGBA color = Gdk::RGBA("red"),
+                       LineStyle line_style = CONTINUOUS,
+                       double line_width = PLOTDATA_DEFAULT_LINE_WIDTH);
+
+      /** Destructor
+       *
+       */
+      virtual ~PlotData2DErrorY();
+
+      /**
+       *
+       * \returns a copy of the dataset Y-values lower error values
+       */
+      std::vector<double> get_vector_error_y_low();
+
+      /**
+       *
+       * \returns a copy of the dataset Y-values higher error values
+       */
+      std::vector<double> get_vector_error_y_high();
+
+      /** Get the data extremes: minima and maxima along both X- and Y-axes
+       *
+       * \param xmin X minimum value
+       * \param xmax X maximum value
+       * \param ymin Y minimum value
+       * \param ymax Y minimum value
+       */
+      virtual void get_extremes(double &xmin, double &xmax, double &ymin, double &ymax) override;
+
+      /** Set the Y-dataset errorbar color
+       *
+       * \param color the color that will used for plotting the Y-data errorbar.
+       */
+      void set_error_y_color(Gdk::RGBA color);
+
+      /** Get the Y-dataset errorbar color
+       *
+       * \return the color that is currently used for plotting the Y-dataset errorbars.
+       */
+      Gdk::RGBA get_error_y_color();
+
+      /** Add a single datapoint, consisting of an \c xval and \c yval value, as well as the errors associated with the \c yval value, to the dataset
+       *
+       * This datapoint will be added at the end of the std::vector's \c x and \c y.
+       * After this method is called, the plot will be automatically updated to reflect the changes.
+       * \param xval an X-value
+       * \param yval an Y-value
+       * \param yval_error_low lower endpoint of the Y-data errorbar
+       * \param yval_error_high upper endpoint of the Y-data errorbar
+       */
+      virtual void add_datapoint(double xval, double yval, double yval_error_low, double yval_error_high);
+
+      /** Method to draw the dataset
+       *
+       * This method is virtual allowing inheriting classes to implement their own method with the same signature.
+       * \param cr the cairo context to draw to.
+       * \param pls the PLplot plstream object that will do the actual plotting on the Cairo context
+       */
+      virtual void draw_plot_data(const Cairo::RefPtr<Cairo::Context> &cr, plstream *pls) override;
+    };
+  }
+}
+
+#endif
